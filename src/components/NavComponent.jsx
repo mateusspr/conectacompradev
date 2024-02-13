7e5b4d611eac8877f099cb986fb90e5e344c3121
Mateus Rodrigues <mateus.pereira.maia94@gmail.com>
2024-02-13 04:02:36 -0300

    feat: aumento da duração de transição

--- src/components/NavComponent.jsx
+++ src/components/NavComponent.jsx
@@ -6,7 +6,7 @@ import 'aos/dist/aos.css'
 const NavComponent = () => {
 
   useEffect(() => {
-    Aos.init({ duration: 2000 })
+    Aos.init({ duration: 3000 })
   }, [])
 
   return (
@@ -17,15 +17,9 @@ const NavComponent = () => {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </div>
           <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-            <li><NavLink>Alexa</NavLink></li>
-            <li>
-              <NavLink>Parent</NavLink>
-              <ul className="p-2">
-                <li><NavLink>Submenu 1</NavLink></li>
-                <li><NavLink>Submenu 2</NavLink></li>
-              </ul>
-            </li>
-            <li><NavLink>Item 3</NavLink></li>
+            <li><NavLink to="/alexa">Alexa</NavLink></li>
+            <li><NavLink to="/left-handed">Canhotos</NavLink></li>
+            <li><NavLink to="/products">Produtos</NavLink></li>
           </ul>
         </div>
         <NavLink to="/" className="btn btn-ghost text-xl">daisyUI</NavLink>
