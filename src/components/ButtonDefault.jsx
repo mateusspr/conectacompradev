86d2bbe7fc218d47b652777d0e516c1d8ad5f663
Mateus Rodrigues <mateuss.prodriguess@gmail.com>
2024-02-13 01:14:17 -0300

    subindo branch main

--- src/components/ButtonDefault.jsx
+++ src/components/ButtonDefault.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const ButtonDefault = () => {
     return (
+        
         <div className='pt-6'>
             <button className="btn neutral">Button</button>
         </div>
